007c784903a1a80122ceac2b292c21fa1ef2f10b
TomasFeeneySligo <TomasFeeneySligo@users.noreply.github.com>
2020-05-13 12:15:44 +0000

    tfS00188023q6

---
+++ TomasFeeney_S00188023/DataManagement/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using TomasFeeney_S00188023;
+
+namespace DataManagement
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            PhoneData db = new PhoneData();
+
+            using (db)
+            {
+                Phone p1 = new Phone("Samsung S20", 500, "Android", "/images/android.png", "/images/s20.jpg");
+            }
+
+        }
+    }
+}
---
+++ TomasFeeney_S00188023/TomasFeeney_S00188023/Phone.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TomasFeeney_S00188023
+{
+    public class Phone
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Operating_System { get; set; }
+        public string OS_Image { get; set; }
+        public string Phone_Image { get; set; }
+        /*
+        public static void IncreasePrice
+        {
+            int percentage;
+        }
+        */
+        public virtual List<Phone> Phones { get; set; }
+
+        public class PhoneData : DbContext
+        {
+            public PhoneData() : base("MyPhoneData") { }
+            public DbSet<Phone> Phones { get; set; }
+        }
+
+        //public List<Phone> Phones { get; set; }
+
+        //public class 
+    }
+}
